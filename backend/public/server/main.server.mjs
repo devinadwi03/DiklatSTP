8550a6d9c903046f1c23cb6370c9d13b92a33ec7
devinadwi03 <devinadwiyanti@mail.ugm.ac.id>
2024-08-29 06:27:49 +0700

    Copy frontend ke backend

--- backend/public/server/main.server.mjs
+++ backend/public/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{c as a}from"./chunk-4UOBSOEV.mjs";import"./chunk-OYED3YUI.mjs";import"./chunk-7I42D7FU.mjs";export{a as default};
+import{c as a}from"./chunk-ZPWBWXWQ.mjs";import"./chunk-OYED3YUI.mjs";import"./chunk-7I42D7FU.mjs";export{a as default};
